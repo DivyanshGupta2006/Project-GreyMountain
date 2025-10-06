ba0d482f69c5c8036faa2a7054e6959bbedd327d
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2025-10-06 23:20:45 +0530

    Fix repo

--- src/main.py
+++ src/main.py
@@ -1,17 +1,6 @@
-from src.backtester import backtest
 from src.update_files import update_data
-from src.utils import get_config
 
-config = get_config.read_yaml()
-
-choice = input("Do you want to update the data: ")
-load_data = choice.lower() == "yes"
-
-if load_data:
-    update_data.update_equity_data()
-
-choice = input("Do you want to backtest the strategy: ")
-backtest_strategy = choice.lower() == "yes"
-
-if backtest_strategy:
-    print(f'Returns: {backtest.backtest_strategy('ABB.NS')}')
+def start():
+    choice = input("Would you like to update the data? (y/n): ")
+    if choice.lower() == 'y':
+        update_data.update()
--- src/update_files/update_data.py
+++ src/update_files/update_data.py
@@ -19,3 +19,22 @@ def update_equity_data():
 
 
     print(f"Finished updating equity data.")
+
+from src.data_processing import download_data, preprocess_data, split_data, feature_engineer, link_data, merge_data
+
+def update():
+    a = input('Download Data? (y/n): ')
+    if a.lower() == 'y':
+        download_data.download()
+    split_data.split()
+    link_data.link('training-val')
+    link_data.link('val-test')
+    feature_engineer.create_features(type='training')
+    feature_engineer.create_features(type='val')
+    feature_engineer.create_features(type='test')
+    preprocess_data.preprocess(type='training')
+    preprocess_data.preprocess(type='val')
+    preprocess_data.preprocess(type='test')
+    merge_data.merge(type='training')
+    merge_data.merge(type='val')
+    merge_data.merge(type='test')
