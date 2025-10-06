0e1b900093d1c54858d669c231584bd185e08cef
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2025-10-06 23:27:42 +0530

    Add more files

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ from src.update_files import update_data
 def start():
     choice = input("Would you like to update the data? (y/n): ")
     if choice.lower() == 'y':
-        update_data.update()
+        update_data.update_equity_data()
--- src/update_files/update_data.py
+++ src/update_files/update_data.py
@@ -1,31 +1,9 @@
-from src.data_processing import download_data, process_data
-from src.utils import get_config, get_absolute_path, check_dir
-
-def update_equity_data():
-    print(f"Updating equity data...")
-    config = get_config.read_yaml()
-    raw_data_dir = get_absolute_path.absolute(config['paths']['raw_data_directory'])
-    processed_data_dir = get_absolute_path.absolute(config['paths']['processed_data_directory'])
-    period = config['data']['max_backtesting_time']
-
-    check_dir.check(raw_data_dir)
-    check_dir.check(processed_data_dir)
-
-    tickers = config['data']['tickers']
-
-    for ticker in tickers:
-        download_data.download_equity_data(ticker, raw_data_dir, period)
-        process_data.process_equity_data(ticker, processed_data_dir)
-
-
-    print(f"Finished updating equity data.")
-
 from src.data_processing import download_data, preprocess_data, split_data, feature_engineer, link_data, merge_data
 
 def update():
     a = input('Download Data? (y/n): ')
     if a.lower() == 'y':
-        download_data.download()
+        download_data.download_equity_data()
     split_data.split()
     link_data.link('training-val')
     link_data.link('val-test')
